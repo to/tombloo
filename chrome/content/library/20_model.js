f1420d6544720f2aa83a32459952ebc371b4807e
ryutaro.kamitsu <ryutaro.kamitsu@6fa8a213-bc4a-0410-921f-31793323fd25>
2008-05-13 10:31:57 +0000

    * Yahooブックマークの公開設定をデフォルト値を反映するように修正
    git-svn-id: https://tombloo.googlecode.com/svn/trunk@54 6fa8a213-bc4a-0410-921f-31793323fd25

--- chrome/content/library/20_model.js
+++ chrome/content/library/20_model.js
@@ -419,21 +419,18 @@ var HatenaStar = {
 }
 
 var YahooBookmarks = {
-	getToken : function(){
-		return doXHR('http://bookmarks.yahoo.co.jp/action/post').addCallback(function(res){
-			return $x('//input[@name="crumbs"]/@value', convertToHTMLDocument(res.responseText));
-		})
-	},
 	post : function(ps){
-		return YahooBookmarks.getToken().addCallback(function(token){
+		return doXHR('http://bookmarks.yahoo.co.jp/action/post').addCallback(function(res){
+			return formContents($x('(id("addbookmark")//form)[1]', convertToHTMLDocument(res.responseText)));
+		}).addCallback(function(fs){
 			return doXHR('http://bookmarks.yahoo.co.jp/action/post/done', {
 				sendContent  : {
 					title      : ps.title,
 					url        : ps.source,
 					desc       : ps.body,
 					tags       : ps.tags? ps.tags.join(' ') : '',
-					crumbs     : token,
-					visibility : ps.private? 0 : 1,
+					crumbs     : fs.crumbs,
+					visibility : ps.private==null? fs.visibility : (ps.private? 0 : 1),
 				},
 			});
 		});

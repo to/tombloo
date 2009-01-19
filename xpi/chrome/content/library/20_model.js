0d7ae8d914de2c7c853f3709e134990b86ee0bae
whym <whym@livedoor.com>
2009-01-19 11:24:44 +0900

    LibraryThing への post を追加

--- xpi/chrome/content/library/20_model.js
+++ xpi/chrome/content/library/20_model.js
@@ -2000,6 +2000,40 @@ models.register({
 	}
 });
 
+models.register({
+	name : 'LibraryThing',
+	ICON : 'http://www.librarything.com/favicon.ico',
+	check : function(ps){
+		return ps.type == 'link' && !ps.file;
+	},
+
+        getAuthCookie : function(){
+            return getCookieString('librarything.com', 'LTAnonSessionID');
+	},
+
+
+	post : function(ps){
+            if (!this.getAuthCookie())
+                throw new Error(getMessage('error.notLoggedin'));
+	    return request('http://www.librarything.com/import_submit.php', {
+		sendContent : {
+                    form_textbox : ps.itemUrl
+	        }
+	    }).addCallback(function(res){
+                var error = res.channel.URI.asciiSpec.extract('http://www.librarything.com/import.php?pastealert=(.*)');
+                if (error)
+                    throw new Error(error);
+		var doc = convertToHTMLDocument(res.responseText);
+		return request('http://www.librarything.com/import_questions_submit.php', {
+		    redirectionLimit : 0,
+		    sendContent : update(formContents(doc), {
+		        masstags :  joinText(ps.tags, ",")
+		    })
+                });
+            });
+	}
+});
+
 
 // 全てのサービスをグローバルコンテキストに置く(後方互換)
 models.copyTo(this);

3bc949cf667d6afb521efb8088efb984310edfb7
to <ryutaro.kamitsu@gmail.com>
2009-06-07 04:27:13 -0400

    * リロードパッチ、デリミタ異常

--- patches/shortcutkey.reloadEnvironment.js
+++ patches/shortcutkey.reloadEnvironment.js
@@ -1,4 +1,4 @@
-shortcutkeys[[KEY_ACCEL, 'SHIFT', 'R'].join('+')] = {
+shortcutkeys[[KEY_ACCEL, 'SHIFT', 'R'].join(' + ')] = {
 	description : 'Reload Tombloo Environment',
 	execute : function(e){
 		cancel(e);

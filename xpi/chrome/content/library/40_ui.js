c491387527543544386726cad23769f1910673ad
to <ryutaro.kamitsu@gmail.com>
2010-01-01 11:57:55 -0500

    クイックポストフォームをタスクバーに表示しないように(dependentを戻した/titlebar=noを省略/Windowsで正常動作)

--- xpi/chrome/content/library/40_ui.js
+++ xpi/chrome/content/library/40_ui.js
@@ -106,7 +106,7 @@ var QuickPostForm = {
 	show : function(ps, position, message){
 		openDialog(
 			'chrome://tombloo/content/quickPostForm.xul', 
-			'chrome,alwaysRaised=yes,resizable=yes,titlebar=no', ps, position, message);
+			'chrome,alwaysRaised=yes,resizable=yes,dependent=yes', ps, position, message);
 	},
 };
 

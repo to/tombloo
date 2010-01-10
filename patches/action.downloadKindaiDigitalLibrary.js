cb4d66bd7cfdefa349808eb290a43caf00601220
to <ryutaro.kamitsu@gmail.com>
2010-01-10 11:10:39 -0500

    近代、細部修正(方向変化対応/詳細情報取得を正確に/複数巻対応)

--- patches/action.downloadKindaiDigitalLibrary.js
+++ patches/action.downloadKindaiDigitalLibrary.js
@@ -5,19 +5,46 @@ Tombloo.Service.actions.register(	{
 		return ctx.host == 'kindai.ndl.go.jp' && ctx.window.top && ctx.window.top.location.href.match('http://kindai.ndl.go.jp/BIImgFrame.php');
 	},
 	execute : function(ctx){
+		// FIXME: 本体へ組み込み
+		function queryString(params, charset){
+			if(isEmpty(params))
+				return '';
+			
+			if(typeof(params)=='string')
+				return params;
+			
+			var qeries = [];
+			var e = (charset)? function(str){
+				return escape((''+str).convertFromUnicode(charset))
+			} : encodeURIComponent;
+			for(var key in params){
+				var value = params[key];
+				if(value==null)
+					continue;
+				
+				if(value instanceof Array){
+					value.forEach(function(val){
+						qeries.push(e(key) + '=' + e(val));
+					});
+				} else {
+					qeries.push(e(key) + '='+ e(value));
+				}
+			}
+			return qeries.join('&');
+		}
+
 		const BAE_URL = 'http://kindai.ndl.go.jp/scrpt/';
 		var self = this;
 		var top = ctx.window.top;
-		var title = $x('//td[@class="titlehead"]', top.frames['W_CONTROL'].document).textContent;
-		var dir = createDir(ctx.host + '/' + title, getDownloadDir());
+		var title = top.frames['W_CONTROL'].document.getElementById('titlearea').textContent.replace(/[\n\r\t 　]+/gm, ' ').trim();
+		var dir = createDir(ctx.host + '/' + validateFileName(title), getDownloadDir());
 		var i = 1;
 		
 		// 書籍詳細情報を保存する
-		request('http://kindai.ndl.go.jp/BIBibDetail.php', {
-			queryString : formContents($x('//form[@name="form"]', top.frames['W_CONTROL'].document))
-		}).addCallback(function(res){
+		var form = formContents($x('//form[@name="form"]', top.frames['W_CONTROL'].document));
+		request('http://kindai.ndl.go.jp/BIBibDetail.php?' + queryString(form, 'EUC-JP')).addCallback(function(res){
 			var doc = convertToHTMLDocument(res.responseText);
-			var detail = $x('//table[7]', doc).textContent;
+			var detail = $x('//table[@width="70%"]', doc).textContent;
 			var file = dir.clone();
 			file.append('detail.txt');
 			putContents(file, 
@@ -26,11 +53,14 @@ Tombloo.Service.actions.register(	{
 		
 		// 全ページ繰り返す
 		deferredForEach($x('(//select)[2]/option/@value', top.frames['W_BODY'].document, true), function(page){
+			page = page.replace(/l=3/, 'l=5').replace(/sz=\d/, 'sz=3');
+			
 			// 画像生成をリクエストする
 			return request(BAE_URL + page).addCallback(function(res){
 				var img = $x('//img[@usemap="#ClickMapPIC"]/@src', convertToHTMLDocument(res.responseText));
 				var file = dir.clone();
 				file.append((i++).pad(4) + '.jpg');
+				
 				return download(BAE_URL + img, file);
 			});
 		}).addCallback(function(){

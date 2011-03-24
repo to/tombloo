bd9a0f4400f963e87f433fe47bf527ff3e1e4963
to <ryutaro.kamitsu@gmail.com>
2011-03-25 06:23:06 +0900

    Ctrl + Fトグルパッチ、Firefox 4対応(thanks lunabahamut)

--- patches/shortcutkey.toggleFindBarVisible.js
+++ patches/shortcutkey.toggleFindBarVisible.js
@@ -1,14 +1,22 @@
-shortcutkeys['CTRL + F'] = {
-	description : 'Toggle Find Bar',
-	execute : function(e){
-		cancel(e);
-		
-		with(getMostRecentWindow()){
-			var bar = document.getElementById('FindToolbar');
-			if(bar.close){
-				bar.hidden? gFindBar.onFindCommand() : gFindBar.close();
-			} else {
-		    bar.hidden? gFindBar.onFindCmd() : gFindBar.closeFindBar();
+let(initialized){
+	shortcutkeys['CTRL + F'] = {
+		description : 'Toggle Find Bar',
+		execute : function(e){
+			if(!initialized){
+				// Firefox 4では一度表示するまでFindToolbarが存在しない
+				initialized = true;
+				return;
+			}
+			
+			cancel(e);
+			
+			with(getMostRecentWindow()){
+				var bar = document.getElementById('FindToolbar');
+				if(bar.close){
+					bar.hidden? gFindBar.onFindCommand() : gFindBar.close();
+				} else {
+			    bar.hidden? gFindBar.onFindCmd() : gFindBar.closeFindBar();
+				}
 			}
 		}
 	}

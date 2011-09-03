e10e581b6e43f18afa7dbde038b6c0d4d77a446b
to <ryutaro.kamitsu@gmail.com>
2011-09-03 14:55:13 +0900

    iTunesストアからAppShopperを検索するアクションを追加

---
+++ patches/action.AppShopper.js
@@ -0,0 +1,11 @@
+Tombloo.Service.actions.register({
+	name : 'AppShopper',
+	type : 'context',
+	icon : 'http://appshopper.com/favicon.ico',
+	check : function(ctx){
+		return ctx.host === 'itunes.apple.com';
+	},
+	execute : function(ctx){
+		addTab('http://appshopper.com/search/?search=' + $x('//div[@id="title"]/h1/text()', ctx.document));
+	},
+}, '----');

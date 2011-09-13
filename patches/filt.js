d08df358db77c5dbe987bdb20382c4714a39d8a3
to <ryutaro.kamitsu@gmail.com>
2011-09-14 03:12:05 +0900

    filt.js(Firefox 6対応/object.watchを廃止/Object.definePropertyでシールするように)

--- patches/filt.js
+++ patches/filt.js
@@ -135,64 +135,41 @@
 	}, '----');
 })();
 
-connect(grobal, 'content-ready', Object.freeze? 
-	function(win){
-		var non = Object.freeze(function(){});
-		var names = 'urchinTracker __utmSetVar pageTracker _gat'.split(' ');
-		names.forEach(function(name){
-			Object.defineProperty(win, name, {value : non});
-		});
-		
-		var pageTracker = Object.freeze({
-			_setDomainName : non,
-			_initData : non,
-			_trackPageview : non,
-			_trackEvent : non,
-		});
-		
-		Object.defineProperty(win, '_gat', {
-			value : Object.freeze({
-				_getTracker : function(){
-					return pageTracker;
-				},
-			})
-		});
-		
-		Object.defineProperty(win, 'pageTracker', {value : pageTracker});
-	} : 
-	function(win){
-		var non = function(){};
-		var names = 'urchinTracker __utmSetVar pageTracker _gat _gaq'.split(' ');
-		names.forEach(function(name){
-			win[name] = non;
-		});
-		
-		var pageTracker = {
-			_setDomainName : non,
-			_initData : non,
-			_trackPageview : non,
-			_trackEvent : non,
-		};
-		win._gat = {
-			_getTracker : function(){
-				return pageTracker;
-			},
-		}
-		win.pageTracker = pageTracker;
-		
-		names.forEach(function(name){
-			fixProp(win, name);
-		});
-		
-		function fixProp(obj, prop){
-			obj.watch(prop, function(key, ov, nv){
-				return ov;
-			});
-			
-			obj = obj[prop];
-			if(typeof(obj)=='object')
-				for(var prop in obj)
-					fixProp(obj, prop);
-		}
+connect(grobal, 'content-ready', function(win){
+	var non = function(){};
+	var names = 'urchinTracker __utmSetVar pageTracker _gat'.split(' ');
+	names.forEach(function(name){
+		win[name] = non;
+	});
+	
+	var pageTracker = {
+		_setDomainName : non,
+		_initData      : non,
+		_trackPageview : non,
+		_trackEvent    : non,
+	};
+	win._gat = {
+		_getTracker : function(){
+			return pageTracker;
+		},
 	}
-);
+	win.pageTracker = pageTracker;
+	
+	names.forEach(function(name){
+		sealProp(win, name);
+	});
+	
+	function sealProp(obj, prop){
+		Object.defineProperty(obj, prop, {
+			configurable : false,
+			enumerable   : false,
+			writable     : false,
+			value        : obj[prop],
+		});
+		
+		obj = obj[prop];
+		if(typeof(obj)=='object')
+			for(var prop in obj)
+				sealProp(obj, prop);
+	}
+});

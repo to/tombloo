6e0a199e9be0d7b7de781289246ba96684e9523e
to <ryutaro.kamitsu@gmail.com>
2012-08-05 17:26:56 +0900

    cleanupLocation、utm_source|cmp追加

--- patches/service.cleanupLocation.js
+++ patches/service.cleanupLocation.js
@@ -1,3 +1,5 @@
 addBefore(Tombloo.Service.extractors, 'extract', function(ctx, ext){
-	ctx.href = ctx.href.replace(/[¥?&;](fr?(om)?|track|ref|FM|ca)=(r(ss(all)?|df)|atom|drs-jp)([&;].*)?/,'');
+	ctx.href = ctx.href
+		.replace(/[¥?&;](fr?(om)?|track|ref|FM|ca)=(r(ss(all)?|df)|atom|drs-jp)([&;].*)?/,'')
+		.replace(/\?(utm_source|cmp)=.*/, '');
 });

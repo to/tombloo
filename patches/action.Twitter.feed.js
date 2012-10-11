ddf9643aefc937496ae49cd28d0bc5fbe51950bc
to <ryutaro.kamitsu@gmail.com>
2012-10-12 02:50:36 +0900

    action.Twitter.feed、Twitter仕様変更に対応

--- patches/action.Twitter.feed.js
+++ patches/action.Twitter.feed.js
@@ -7,6 +7,6 @@ Tombloo.Service.actions.register({
 	},
 	execute : function(ctx){
 		var user = ctx.document.querySelector('.screen-name').textContent.replace('@', '');
-		addTab('http://twitter.com/statuses/user_timeline/' + user + '.rss');
+		addTab('https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=' + user);
 	},
 }, '----');
